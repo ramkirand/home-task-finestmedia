3eecc1360c22b659e3466502a24622b8d7548dcc
KIRAN <ramkirand@gmail.com>
2021-05-18 01:22:28 +0530

    Added Serializable

--- src/main/java/model/ConsumptionData.java
+++ src/main/java/model/ConsumptionData.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,7 +16,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Entity
-public class ConsumptionData {
+public class ConsumptionData implements Serializable{
+
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
